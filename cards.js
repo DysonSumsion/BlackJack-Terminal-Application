6ebd6292c142dc5d2744182a960362d2af0cf150
Sukianto <sukialiong@gmail.com>
2019-09-04 14:48:51 +1000

    cards data

---
+++ cards.js
@@ -0,0 +1,264 @@
+const cards = [
+  {
+    type: "Ace",
+    symbol: "Spade",
+    value: 11
+  },
+  {
+    type: "Two",
+    symbol: "Spade",
+    value: 2
+  },
+  {
+    type: "Three",
+    symbol: "Spade",
+    value: 3
+  },
+  {
+    type: "Four",
+    symbol: "Spade",
+    value: 4
+  },
+  {
+    type: "Five",
+    symbol: "Spade",
+    value: 5
+  },
+  {
+    type: "Six",
+    symbol: "Spade",
+    value: 6
+  },
+  {
+    type: "Seven",
+    symbol: "Spade",
+    value: 7
+  },
+  {
+    type: "Eight",
+    symbol: "Spade",
+    value: 8
+  },
+  {
+    type: "Nine",
+    symbol: "Spade",
+    value: 9
+  },
+  {
+    type: "Ten",
+    symbol: "Spade",
+    value: 10
+  },
+  {
+    type: "Jack",
+    symbol: "Spade",
+    value: 10
+  },
+  {
+    type: "Queen",
+    symbol: "Spade",
+    value: 10
+  },
+  {
+    type: "King",
+    symbol: "Spade",
+    value: 10
+  },
+  {
+    type: "Ace",
+    symbol: "Heart",
+    value: 11
+  },
+  {
+    type: "Two",
+    symbol: "Heart",
+    value: 2
+  },
+  {
+    type: "Three",
+    symbol: "Heart",
+    value: 3
+  },
+  {
+    type: "Four",
+    symbol: "Heart",
+    value: 4
+  },
+  {
+    type: "Five",
+    symbol: "Heart",
+    value: 5
+  },
+  {
+    type: "Six",
+    symbol: "Heart",
+    value: 6
+  },
+  {
+    type: "Seven",
+    symbol: "Heart",
+    value: 7
+  },
+  {
+    type: "Eight",
+    symbol: "Heart",
+    value: 8
+  },
+  {
+    type: "Nine",
+    symbol: "Heart",
+    value: 9
+  },
+  {
+    type: "Ten",
+    symbol: "Heart",
+    value: 10
+  },
+  {
+    type: "Jack",
+    symbol: "Heart",
+    value: 10
+  },
+  {
+    type: "Queen",
+    symbol: "Heart",
+    value: 10
+  },
+  {
+    type: "King",
+    symbol: "Heart",
+    value: 10
+  },
+  {
+    type: "Ace",
+    symbol: "Clover",
+    value: 11
+  },
+  {
+    type: "Two",
+    symbol: "Clover",
+    value: 2
+  },
+  {
+    type: "Three",
+    symbol: "Clover",
+    value: 3
+  },
+  {
+    type: "Four",
+    symbol: "Clover",
+    value: 4
+  },
+  {
+    type: "Five",
+    symbol: "Clover",
+    value: 5
+  },
+  {
+    type: "Six",
+    symbol: "Clover",
+    value: 6
+  },
+  {
+    type: "Seven",
+    symbol: "Clover",
+    value: 7
+  },
+  {
+    type: "Eight",
+    symbol: "Clover",
+    value: 8
+  },
+  {
+    type: "Nine",
+    symbol: "Clover",
+    value: 9
+  },
+  {
+    type: "Ten",
+    symbol: "Clover",
+    value: 10
+  },
+  {
+    type: "Jack",
+    symbol: "Clover",
+    value: 10
+  },
+  {
+    type: "Queen",
+    symbol: "Clover",
+    value: 10
+  },
+  {
+    type: "King",
+    symbol: "Clover",
+    value: 10
+  },
+  {
+    type: "Ace",
+    symbol: "Diamond",
+    value: 11
+  },
+  {
+    type: "Two",
+    symbol: "Diamond",
+    value: 2
+  },
+  {
+    type: "Three",
+    symbol: "Diamond",
+    value: 3
+  },
+  {
+    type: "Four",
+    symbol: "Diamond",
+    value: 4
+  },
+  {
+    type: "Five",
+    symbol: "Diamond",
+    value: 5
+  },
+  {
+    type: "Six",
+    symbol: "Diamond",
+    value: 6
+  },
+  {
+    type: "Seven",
+    symbol: "Diamond",
+    value: 7
+  },
+  {
+    type: "Eight",
+    symbol: "Diamond",
+    value: 8
+  },
+  {
+    type: "Nine",
+    symbol: "Diamond",
+    value: 9
+  },
+  {
+    type: "Ten",
+    symbol: "Diamond",
+    value: 10
+  },
+  {
+    type: "Jack",
+    symbol: "Diamond",
+    value: 10
+  },
+  {
+    type: "Queen",
+    symbol: "Diamond",
+    value: 10
+  },
+  {
+    type: "King",
+    symbol: "Diamond",
+    value: 10
+  }
+]
+
+console.log(cards.length);
