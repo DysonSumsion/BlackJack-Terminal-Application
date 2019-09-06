63b568d88e59b2d91809f37bb8440b510c59f5b1
Sukianto Suteja <42060507+sukibeww@users.noreply.github.com>
2019-09-06 13:57:29 +1000

    added hit and stand function

--- blackjack.js
+++ blackjack.js
@@ -1,14 +1,57 @@
 const cards = require('./cards')
 const readlineSync = require('readline-sync');
 let option = ["Deal", "Quit"]
-const hitOrStand = () => {
+const getTotal = (hand) => {
+    handTotal = hand.reduce((total, currentHand) => {
+        total += currentHand.value
+        return total
+    }, 0);
+    return handTotal;
+}
+const stand = (cards, playerHand, dealerHand) => {
+    while(getTotal(dealerHand) <= 17){
+        dealerHand.push(cards.deck.deal())
+        console.log("Dealer hand: ");
+        console.log(dealerHand);
+        console.log(`Total: ${getTotal(dealerHand)}`);
+    }
+    if(getTotal(dealerHand) > 21){
+        console.log("Dealer Bust!");
+    }
+    else if(getTotal(dealerHand) == getTotal(playerHand)){
+        console.log("Push");
+    }
+    else if(getTotal(dealerHand) > getTotal(playerHand)){
+        console.log("Dealer win!");
+    }
+    else{
+        console.log("Player win!");
+    }
+}
+const hitOrStand = (cards, hand) => {
+    const hit = (cards, hand) => {
+        hand.push(cards.deck.deal());
+        console.log("Player Hand :");
+        console.log(playerHand);
+        playerTotal = getTotal(hand)
+        console.log(`Player total: ${playerTotal}`);
+        if(playerTotal == 21){
+            stand()
+        }
+        else if(playerTotal < 21){
+            hitOrStand(cards, hand)
+        }
+        else{
+            console.log("Bust!, Dealer win");
+        }
+    }
     input = readlineSync.keyInSelect(["Hit", "Stand"], "Would you like to hit or stand? ")
     switch(input){
         case 0:
-            hit();
+            hit(cards, hand);
             break;
         case 1:
-            stand();
+            stand(cards, playerHand, dealerHand);
             break;
         case -1:
             console.log("bye");
@@ -24,19 +67,13 @@ const play = () => {
     dealerHand.push(cards.deck.deal());
     console.log("Dealer hand ( 1 not shown )");
     console.log(dealerHand[0]);
-    dealerTotal = dealerHand.reduce((total, currentHand) => {
-        total += currentHand.value
-        return total
-    }, 0)
+    dealerTotal = getTotal(dealerHand);
     console.log(`Dealer total: ${dealerHand[0].value}`);
     console.log("Player Hand :");
     console.log(playerHand);
-    playerTotal = playerHand.reduce((total, currentHand) => {
-        total += currentHand.value
-        return total
-    }, 0)
+    playerTotal = getTotal(playerHand);
     console.log(`Player total: ${playerTotal}`);
-    hitOrStand()
+    hitOrStand(cards, playerHand)
 }
 const main = () => {
     let input = readlineSync.keyInSelect(option, "Menu")
@@ -45,7 +82,7 @@ const main = () => {
             play();
             break;
         case 1:
-            console.log("Stand");
+            stand();
             break;
         case -1:
             console.log("Quit");
